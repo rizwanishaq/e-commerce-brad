2b316630debceb34e2d79ca2cf6a57d0a08fdc44
rizwanishaq <rizwanishaq@gmail.com>
2020-10-05 17:32:55 +0200

    cart price subtotal all done

--- frontend/src/contexts/cartContext.js
+++ frontend/src/contexts/cartContext.js
@@ -4,24 +4,37 @@ import axios from "axios";
 export const CartContext = createContext();
 
 const CartContextProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(
+    localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : []
+  );
 
   useEffect(() => {
-    const cartItemsFromStorage = localStorage.getItem("cartItems")
-      ? JSON.parse(localStorage.getItem("cartItems"))
-      : [];
-
-    setCartItems(cartItemsFromStorage);
-  }, []);
-
-  const addItems = async (id, qty) => {
-    const { data } = await axios.get(`/api/products/${id}`);
-    setCartItems([...cartItems, data]);
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  const addCartItem = async (id, qty) => {
+    const { data } = await axios.get(`/api/products/${id}`);
+    const existItem = cartItems.find((item) => item._id === id);
+    if (existItem) {
+      existItem.qty = qty;
+      setCartItems(
+        cartItems.map((item) => (item._id === id ? existItem : item))
+      );
+    } else {
+      const newItem = data;
+      newItem.qty = qty;
+      setCartItems([...cartItems, newItem]);
+    }
+  };
+
+  const removeCartItem = (id) => {
+    setCartItems(cartItems.filter((x) => x._id !== id));
   };
 
   return (
-    <CartContext.Provider value={{ cartItems, addItems }}>
+    <CartContext.Provider value={{ cartItems, addCartItem, removeCartItem }}>
       {children}
     </CartContext.Provider>
   );
